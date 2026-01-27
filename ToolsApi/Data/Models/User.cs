16f37d5dfe6a7b1c20b97c6c479d0cbb062c2d48
Mochammad Lutfi Bintoro Syaiful <lutfibintoro@gmail.com>
2026-01-27 23:29:16 +0700

    configure mysql database

---
+++ ToolsApi/Data/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ToolsApi.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string WhatsAppNumber { get; set; } = null!;
+    }
+}
---
+++ ToolsApi/Data/ToolsApiDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToolsApi.Data.Models;
+
+namespace ToolsApi.Data
+{
+    public class ToolsApiDbContext : DbContext
+    {
+        public ToolsApiDbContext(DbContextOptions<ToolsApiDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(buildAction =>
+            {
+                buildAction.HasKey(e => e.Id);
+                buildAction.Property(e => e.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+                buildAction.Property(e => e.WhatsAppNumber).IsRequired(true);
+            });
+        }
+    }
+}
