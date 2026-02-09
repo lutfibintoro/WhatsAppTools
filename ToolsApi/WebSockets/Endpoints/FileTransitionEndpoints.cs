05cab57692814e93502afb84c01829f3c9808d16
Mochammad Lutfi Bintoro Syaiful <lutfibintoro@gmail.com>
2026-02-09 17:58:00 +0700

    endpoint ws utama

---
+++ ToolsApi/WebSockets/Endpoints/FileTransitionEndpoints.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.WebSockets;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using ToolsApi.Models.Dto.Response;
+using ToolsApi.WebSockets.Services.Interface;
+
+namespace ToolsApi.WebSockets.Endpoints
+{
+    public static class FileTransitionEndpoints
+    {
+        public static WebApplication MapFileTransitionEndpoints(this WebApplication app)
+        {
+            app.MapGet("/ws/file/transition", async Task
+                (HttpContext context,
+                [FromQuery] int? phoneNumber,
+                [FromServices] ILogger<Program> logger,
+                [FromServices] ITransmisiManagerService transmisiManagerService) =>
+            {
+                if (!context.WebSockets.IsWebSocketRequest)
+                    throw new WebSocketException("upgrade protokol ke websocket");
+
+                using WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                await transmisiManagerService.HandshakeAsync(webSocket);
+                await transmisiManagerService.IdleAsync();
+            });
+
+
+            return app;
+        }
+    }
+}
